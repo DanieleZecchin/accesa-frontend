dd5419d387bf0eaf2af21bccad7ec0a0ec01f328
Francesco Raguso <fraguso@alpenite.com>
2019-07-23 17:02:26 +0200

    New check valid email in forgot page

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ $(function() {
       $('.cc_login_form').addClass('validation-error');
     }
 
+    // Forgot password page - check valid email
+    if ($('.cc_forgot_password_panel').length) {
+
+      $('.cc_forgot_password_panel input[type="text"]').on('keyup', function() {
+
+        if (!isValidEmail($('.cc_forgot_password_panel input[type=text]').val()))
+          $('.cc_forgot_password_panel input[type=submit]').prop('disabled', true);
+        else
+          $('.cc_forgot_password_panel input[type=submit]').prop('disabled', false);
+      });
+    }
+
 });
+
+function isValidEmail(email)
+{
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
