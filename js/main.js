59a900a3c0d7bb50cdeca3d11e3befb6be29a093
DanieleZecchin <dzecchin@alpenite.com>
2019-07-23 11:44:06 +0200

    Fixed validation error behavior

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log('custom javascript loaded');
 $(function() {
 
     // Login validation error
-    if ($('#login_main_content .messagingSection-Error').length > 0) {
+    if ($('#login_main_content .messagingSection-Error').is(':visible')) {
       $('.cc_login_form').addClass('validation-error');
     }
 
