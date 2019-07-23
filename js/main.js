65b47e9a1b569e6f05363b48fe60c7d9eafd9ea2
DanieleZecchin <dzecchin@alpenite.com>
2019-07-23 11:20:03 +0200

    Validation error at login changes input box color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log('custom javascript loaded');
+
+// Login validation error
+if ($('#login_main_content .messagingSection-Error').length > 0) {
+  $('.cc_login_form').addClass('validation-error');
+}
