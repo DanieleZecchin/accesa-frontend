15f1aac5405696cc646fa2bab0fe05a2ff57d066
Francesco Raguso <fraguso@alpenite.com>
2019-07-27 11:26:08 +0200

    [Fix] Hide Account

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(function() {
     }
 
     // Hide Account in Top Navbar if not logged in
-    if ($('a.goToLogin.cc_goto_login').length) {
+    if ($('body').find('a.goToLogin.cc_goto_login').length) {
 
       $('li.cc_my_account').hide();
     }
